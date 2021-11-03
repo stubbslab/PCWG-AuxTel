ef02b00ea830fdaa4bae75fd471f9a2844fc4856
empEvil <emp160330@utdallas.edu>
2021-11-03 14:58:52 -0400

    adding an option to look at skewed flux

--- Findingcircles.py
+++ Findingcircles.py
@@ -9,7 +9,7 @@ from lsst.rapid.analysis.imageExaminer import ImageExaminer
 import os
 
 
-def findCircles(day_obs, seq_nums, doPlot=False, config=None, path=None, **kwargs):
+def findCircles(day_obs, seq_nums, doPlot=False, planeSkew=False, config=None, path=None, **kwargs):
     """Let's find all the circles! this function simply loops over a
     sequence list (seq_List) the function FindCircle, which does all
     the work for a single exposure.
@@ -30,6 +30,9 @@ def findCircles(day_obs, seq_nums, doPlot=False, config=None, path=None, **kwarg
         Optional configuration dictionary, if not provided we will use a
         standard configuration defined below.
 
+    path : `string`
+        Optional path where detail plots should be saved. 
+
     Returns
     -------
     efd_infos : `list of touples`
@@ -40,11 +43,16 @@ def findCircles(day_obs, seq_nums, doPlot=False, config=None, path=None, **kwarg
 
     dys : `list`
         The displacement along the y-axis for all exposures.
+    
+    coefficients : `list`
+        Optional list of coefficients for flux skew. 
     """
     butler = dafPersist.Butler('/project/shared/auxTel/rerun/quickLook')
     # efd_infos = []
     dxs = []
     dys = []
+    if planeSkew:
+        coef = []
 
     # Here we look if a config dictionary is passed, if not then we will
     # use the default values.
@@ -74,7 +82,11 @@ def findCircles(day_obs, seq_nums, doPlot=False, config=None, path=None, **kwarg
         outer_circle = np.zeros(3, dtype=int)
         inner_circle = np.zeros(3, dtype=int)
         exp = butler.get('quickLookExp', dayObs=day_obs, seqNum=seq_num)
-        outer_circle, inner_circle = findCircle(exp, config, seq_num, path, doPlot)
+        if planeSkew:
+            outer_circle, inner_circle, coefficients = findCircle(exp, config, seq_num, path, doPlot, planeSkew)
+            coef.append(coefficients)
+        else:    
+            outer_circle, inner_circle = findCircle(exp, config, seq_num, path, doPlot)
         centration_offset = outer_circle - inner_circle
         print(f"Seq_num: {seq_num}, dx_offset={centration_offset[0,0]}, dy_offset={centration_offset[0,1]}")
         # expId, position = get_efd_info(obs_Date, seq_Num)
@@ -82,11 +94,13 @@ def findCircles(day_obs, seq_nums, doPlot=False, config=None, path=None, **kwarg
         # efd_infos.append([expId, position])
         dxs.append(centration_offset[0, 0])
         dys.append(centration_offset[0, 1])
+    if planeSkew:
+        return dxs, dys, coef 
+    else:
+        return dxs, dys
 
-    return dxs, dys
 
-
-def findCircle(exp, config, seqNum, path, doPlot=False):
+def findCircle(exp, config, seqNum, path, doPlot=False, planeSkew=False):
     """This function does all the tricks to find the circle for a single exposure
     and returns the inner and outer circles for it.
 
@@ -108,6 +122,9 @@ def findCircle(exp, config, seqNum, path, doPlot=False):
     doPlot : 'bool'
         Boolean, whether we should do all the extra plotting or not.
 
+    planeSkew : 'bool'
+        Boolean, wheter or not we should attempt to fit the flux to a plane.
+
     Returns
     -------
     outer_circle : 'list'
@@ -117,6 +134,9 @@ def findCircle(exp, config, seqNum, path, doPlot=False):
     inner_circle : 'list'
         list consisting of the centroid position (x,y) and the radius of the inner
         circle of the donut.
+
+    (Optional) plane_coefficient : 'list'
+        list of the coefficients (C) needed to plot a plane z = C[0]*x = C[1]*y + C[2]
     """
     path = os.path.join(path, f"seq{seqNum:05}")
     if doPlot:
@@ -135,14 +155,16 @@ def findCircle(exp, config, seqNum, path, doPlot=False):
 
     print("We cut out the figure, next lets smooth it", flush=True)
 
-    norm_image = _smoothNormalized(cutout, config, path, doPlot)
+    norm_image, cutout_smoothed = _smoothNormalized(cutout, config, path, doPlot)
 
     # Now we have the normalized image, and we want to convert those to either being there or not
 
     int_image = _detectMask(norm_image, config, path, doPlot)
 
-    # Now calling cv2 to find the actual circles using a HoughCircles.
+    # Now calling cv2 to find the actual circles using a HoughCircles transform.
 
+    # Here, param1 and param2 are related to to how the cv2 method finds the edges of the circle, 
+    # and the center respectively. 
     params_big = {'param1': 10, 'param2': 10, 'minRadius': int(config['outer_radius']),
                   'maxRadius': int(1.2*config['outer_radius'])}
     params_small = {'param1': 30, 'param2': 10, 'minRadius': int(config['inner_radius']),
@@ -170,7 +192,11 @@ def findCircle(exp, config, seqNum, path, doPlot=False):
         ax2.imshow(int_image, origin='lower')
         fig.savefig(path)
 
-    return outer_circle, inner_circle
+    if planeSkew:
+        plane_coefficient = _planeskew(cutout_smoothed, norm_image, config, path, doPlot)
+        return outer_circle, inner_circle, plane_coefficient
+    else:
+        return outer_circle, inner_circle
 
 
 def _examine(exp, config, path, doPlot=False):
@@ -211,7 +237,7 @@ def _smoothNormalized(cutout, config, path, doPlot=False):
         ax3.plot(normImage[config['Halfbox'], :])
         fig.savefig(path)
 
-    return normImage
+    return normImage, cutoutSmoothed
 
 
 def _detectMask(normImage, config, path, doPlot):
@@ -227,12 +253,38 @@ def _detectMask(normImage, config, path, doPlot):
 
     return intImage
 
-
 def _applyHoughTransform(intImage, min_dist, params):
     circle = cv2.HoughCircles(intImage, cv2.HOUGH_GRADIENT, 1, min_dist, **params)
 
     circle = np.round(circle[0, :]).astype(int)
     return circle
+
+def _planeskew(smoothedImage, normImage, config, path, doPlot):
+    """ What if we wanted to also find the skew, of the flux plane? Here we attempt to fit
+    a plane to the normalized fluxes, so in case we do see that the flux is uneven accross
+    the image, we have the coefficients for a z = a*x + b*y + c plane. with z being the flux.
+    This implementation is heavily inspired by the example given in 
+    https://gist.github.com/amroamroamro/1db8d69b4b65e8bc66a6
+    """
+    nmi = ma.masked_less_equal(normImage, config["maxclip"])
+
+    if doPlot:
+        path = os.path.join(path, "cutout.png")
+        fig = plt.figure()
+        plt.imshow(nmi, origin = 'lower')
+        plt.colorbar()
+        fig.savefig(path)
+
+    grid = np.indices(nmi.shape)
+    mask = ma.getmask(nmi)
+    y = ma.array(grid[0], mask = mask)
+    x = ma.array(grid[1], mask = mask)
+    z = ma.array(smoothedImage, mask = mask)
+    data = np.c_[x.compressed(),y.compressed(),z.compressed()]
+    A = np.c_[data[:,0],data[:,1],np.ones(data.shape[0])]
+    import scipy.linalg as linalg
+    C,_,_,_ = linalg.lstsq(A,data[:,2])
+    return C
 
 
 def _getEfdData(client, dataSeries, startTime, endTime):
